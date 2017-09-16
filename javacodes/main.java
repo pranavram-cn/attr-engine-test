1f35ea09e3770e7119e1c20b6dc96e1e7eda690e
Pranav Ram <pranav.ram@codenation.co.in>
2017-09-16 12:36:23 +0400

    Commit one

---
+++ javacodes/main.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+    public static String author = "Akshat";
+
+
+    public static void main(String[] args){
+        System.out.println("In the main function");
+    }
+
+    public void longMethod(){
+        int a = 10;
+        int b = 5;
+        float c = a/(float)b;
+        System.out.println(c);
+
+        float d = 2;
+
+        if(d == c){
+            System.out.println("The two floats are equal");
+        } else {
+            System.out.println("The two floats are not equal at all");
+        }
+
+        for(int i = 0; i<a; i++){
+            System.out.print("Doing Nothing");
+        }
+
+        int x = 0;
+
+        List list = new ArrayList<Integer>();
+
+//        while(true){
+//            x++;
+//        }
+
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+        System.out.println("123");
+    }
+}
